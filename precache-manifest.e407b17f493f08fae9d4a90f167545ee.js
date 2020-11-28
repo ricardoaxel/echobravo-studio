1520c60bc4f2b0a8a8f0b65bdbd8b3ab8e4ece4b
Ashel <elchetumal@gmail.com>
2020-11-28 16:37:45 -0600

    Updates

---
+++ precache-manifest.e407b17f493f08fae9d4a90f167545ee.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b70164f538482b040f7d6e5ef3a498f",
+    "url": "/echobravo-studio/index.html"
+  },
+  {
+    "revision": "f20de26e6ea5799d24f1",
+    "url": "/echobravo-studio/static/css/main.908d595a.chunk.css"
+  },
+  {
+    "revision": "b7448c4d8257857a6f1d",
+    "url": "/echobravo-studio/static/js/2.1b21adc6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/echobravo-studio/static/js/2.1b21adc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f20de26e6ea5799d24f1",
+    "url": "/echobravo-studio/static/js/main.739f588f.chunk.js"
+  },
+  {
+    "revision": "2400ffabb2f94cbf1960",
+    "url": "/echobravo-studio/static/js/runtime-main.e0280e31.js"
+  },
+  {
+    "revision": "6757530e2fb8e4e2e55f861d427c38c5",
+    "url": "/echobravo-studio/static/media/aboutme.6757530e.jpeg"
+  },
+  {
+    "revision": "678b4e5369855a792f9f87ba836e924c",
+    "url": "/echobravo-studio/static/media/emailBlack.678b4e53.png"
+  },
+  {
+    "revision": "3feee8daa83345b8a42e537e1046253a",
+    "url": "/echobravo-studio/static/media/fbBlack.3feee8da.png"
+  },
+  {
+    "revision": "97bcac2bdb50c676d9569944c055fd92",
+    "url": "/echobravo-studio/static/media/instaBlack.97bcac2b.png"
+  },
+  {
+    "revision": "eeed2f16021ec1a2228401596414a4a6",
+    "url": "/echobravo-studio/static/media/whatsappBlack.eeed2f16.png"
+  }
+]);
